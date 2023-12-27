99f722407cfa20275d5d1478fa52b648e6b64a6c
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-12-27 10:26:09 -0400

    Operators of Comparation on py

---
+++ Operadores/op_comparacion.py
@@ -0,0 +1,18 @@
+suma = 10 * 20 
+potencia  = 3 **3
+
+a = 2
+b = 2
+c = 5
+
+value  =  b == a
+
+igual_igual = 4 == 5
+distinto_que  = 6 != 6
+mayor_que = 8 > 9
+menor_que = 10 < 11
+menor_o_igual_que = 10 <= 9
+mayor_o_igual_que = 2 >=2
+
+
+print(f"=={igual_igual}\n >={mayor_o_igual_que}")
