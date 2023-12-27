bed121f8e7b88cb01677dcc90a7c7d08d52200ce
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-12-27 10:18:40 -0400

    Adding Folder for Operators on py

---
+++ Operadores/op_aritmeticos.py
@@ -0,0 +1,19 @@
+#Suma y resta
+suma = 7 + 13
+resta = 28 - 8
+#Multiplicacion y division
+mult = 20 * 2
+div = 12 / 5
+#Potenciacion
+exponente = 12**5
+#Divison baja
+division_baja = 12//5
+# Mod
+Modulo = 12 % 2
+
+
+#Type() devuelve el tipo de dato.
+
+print(division_baja,Modulo,div,suma)
+print(type(div))
+ 
