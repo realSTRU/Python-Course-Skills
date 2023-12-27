35f27c8d92b26e9232257bcdde4f652afa80c6b8
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-12-27 10:54:04 -0400

    Operators of conditionals.py

---
+++ Operadores/op_condicionales.py
@@ -0,0 +1,36 @@
+edad_persona1 = 18
+edad_persona2 = 17
+if edad_persona2 >= 18:
+    print("Eres mayor de edad")
+    print("puedes verte un hentai")
+else:
+    print("No puedes ver hentai")
+    print("eres menor de edad")
+    
+    
+sueldo = 43000
+
+if(sueldo == 12000):
+    print("Probablemente estes trabajando donde Gregorai")
+elif(sueldo < 12000):
+    print("probablemente estes trabajando donde lo vampiro")
+else:
+    print("manda el link para mandar el CV")
+
+
+#I´m just kidding
+gastos = 37000
+
+if sueldo > 10000:
+    if sueldo - gastos < 0:
+        print("No te estas admisnistrando bien")
+    elif sueldo -  gastos >= 2000:
+        print("Eres un buen muchacho")
+    else:
+        print("vamos a ver como te las arreglas")
+elif (sueldo < 10000) and (sueldo >= 2000):
+    print("no estas muy bien que digamos muchacho")
+else:
+    print("mudate para atabalero") 
+        
+
