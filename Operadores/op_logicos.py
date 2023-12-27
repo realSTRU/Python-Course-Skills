1024308a42a7375074c9402d7f80101ec079d5db
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-12-27 11:15:24 -0400

    Logic_Operators.py

---
+++ Operadores/op_logicos.py
@@ -0,0 +1,14 @@
+#AND operator
+value_for_true_and = True & True 
+value_for_false_and = False & True
+
+#OR operator
+value_for_true_or = False | True
+value_for_false_or = False | False
+
+#NOT operator
+
+contra_value_true = not True
+contra_value_false = not False
+
+print(value_for_true_and, value_for_false_and, value_for_true_or, value_for_false_or,contra_value_true, contra_value_false)
