a69eb8bc8b7ef3a004a2942fad86f999c1509d81
Cesar Reynoso <cesarreynoso0969@gmail.com>
2024-01-02 11:14:03 -0400

    metodos_diccionarios.py

---
+++ Metodos/metodos_diccionarios.py
@@ -0,0 +1,17 @@
+diccionario = {
+    "Nombre" : "Cesar",
+    "Apellidodo" : "Reynoso",
+    "Hobby" : "Basketball"
+}
+
+claves = diccionario.keys()
+
+print(claves)
+
+print(diccionario.get("Hobby"))
+print(diccionario["Nombre"])
+
+diccionario_iterable = diccionario.items()
+print(diccionario)
+print(diccionario_iterable)
+
